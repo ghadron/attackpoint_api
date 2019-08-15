1ec470ba47f49a9c8e6ca1571e958ecd7039ee4f
Graeme Farrand <gglfarrand@gmail.com>
2019-08-15 13:23:00 -0400

    Finished init_attackpoint

--- attackpoint_api.R
+++ attackpoint_api.R
@@ -1,19 +1,42 @@
 library(RSelenium)
 
-init_attackpoint <- function(username, password) {
+#' Initializes a session in a user's account on Attackpoint
+#' 
+#'
+#' @param username A String
+#' @param password A String
+#'
+#' @return
+#' @export
+#' 
+#' @examples
+init_attackpoint <- function(username, password, docker_port = 4445L) {
   if(missing(username) || is.null(username)) error("username required")
   if(missing(password) || is.null(password)) error("password required")
   
-  system("docker run --name attackpoint_container -d -p 4445:4444 selenium/standalone-firefox:2.53.1")
+  init_rem_dr <- remoteDriver(remoteServerAddr = "localhost", 
+                              port = docker_port, browserName = "firefox")
+  init_rem_dr$open()
+  rem_dr <<- init_rem_dr
   
-  rem_dr <- remoteDriver(remoteServerAddr = "localhost", port = 4445L, 
-                          browserName = "firefox")
-  rem_dr$open()
-  
+  if(try_ap_login(username, password)) username
+  else "Login Fail"
+}
+
+try_ap_login <- function(username, password) {
   rem_dr$navigate("https://www.attackpoint.org/login.jsp?returl=https%3A%2F%2Fwww.attackpoint.org%2F")
 
-  print(rem_dr$getCurrentUrl())  
+  rem_dr$findElement(using = "name", "username")$
+    sendKeysToElement(list(username))
+  rem_dr$findElement(using = "name", "password")$
+    sendKeysToElement(list(password, key = "enter"))
+  
+  rem_dr$getCurrentUrl() == "https://www.attackpoint.org/"
 }
+
+#' username: Hadron Helper
+#' password: copper12
+#'
 
 close_attackpoint <- function() {
   
