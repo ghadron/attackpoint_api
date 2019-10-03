64de081494ddec1a9e87468f05563a5b0da54675
Graeme Farrand <gglfarrand@gmail.com>
2019-10-03 14:11:27 -0400

    Refactor

--- ap_selenium.R
+++ ap_selenium.R
@@ -26,8 +26,7 @@ init_remDr <- function(docker_port = 4445L, browser_name = "firefox") {
 try_login <- function(username, password) {
   remDr$navigate("https://www.attackpoint.org/login.jsp?returl=https%3A%2F%2Fwww.attackpoint.org%2F")
   
-  remDr$findElement("name", "username")$
-    sendKeysToElement(list(username))
+  remDr$findElement("name", "username")$sendKeysToElement(list(username))
   remDr$findElement("name", "password")$
     sendKeysToElement(list(password, key = "enter"))
   
